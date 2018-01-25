834075d9edbe94cc07436bc6685fe602baed2761
Raging-Developer <raging.development@gmail.com>
2018-01-25 21:46:54 +0000

    memory leaks
    Changed the async to a static class and use weak references all to
    prevent memory leaks

--- java/app/personal_weather/Yahoo_feed.java
+++ java/app/personal_weather/Yahoo_feed.java
@@ -3,6 +3,7 @@ package app.personal_weather;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -21,8 +22,8 @@ import app.personal_weather.data.Channel;
  */
 class Yahoo_feed
 {   
-    private Exception error;  
-    private Weather_Activity weather;  
+    private static Exception error;
+    private Weather_Activity this_weather;
     
     /**
      * Query the yahoo api and return a JSON of the endpoint. 
@@ -31,79 +32,88 @@ class Yahoo_feed
     Yahoo_feed(Weather_Activity weather)
     {
         super();
-        this.weather = weather;
+        this_weather = weather;
     }
 
     /**
      * Queries the weather server and loads up the JSON object from an async task
+     * that now use static classes and WeakReferences to prevent memory leaks.
      * @param new_location String
      */
     void refresh (String new_location)
-    {        
-        new AsyncTask<String, Void, String>()        
+    {
+        My_async task = new My_async(this_weather);
+        task.execute(new_location);
+    }
+    private static class My_async extends AsyncTask<String, Void, String>
+    {
+        private WeakReference<Weather_Activity> weak_ref;
+
+        My_async(Weather_Activity weather)
         {
-            @Override protected String doInBackground(String... params)            
-            {                                
-                //The query and endpoint are taken from the yahoo developer site.
-                String query = String.format("select * from weather.forecast where woeid in "
-                                             +"(select woeid from geo.places(1) where text=\"%s\") and u='c'", params[0]);
-                
-                String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json",
-                                                 Uri.encode(query));                  
-                                                                
-                try
-                {
-                    URL           url   = new URL(endpoint);
-                    URLConnection conn  = url.openConnection();
-                    InputStream   input = conn.getInputStream();
+            weak_ref = new WeakReference<>(weather);
+        }
 
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                    StringBuilder  result = new StringBuilder();
-                    String line;
-                    
-                    while ((line = reader.readLine()) != null)
-                    {
-                        result.append(line);
-                    }
-                    return result.toString();
-                }
-                catch (Exception e)
+        @Override protected String doInBackground(String... params)
+        {
+            //The query and endpoint are taken from the yahoo developer site.
+            String query = String.format("select * from weather.forecast where woeid in "
+                                         +"(select woeid from geo.places(1) where text=\"%s\") and u='c'", params[0]);
+
+            String endpoint = String.format("https://query.yahooapis.com/v1/public/yql?q=%s&format=json",
+                                             Uri.encode(query));
+
+            try
+            {
+                URL           url   = new URL(endpoint);
+                URLConnection conn  = url.openConnection();
+                InputStream   input = conn.getInputStream();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                StringBuilder  result = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null)
                 {
-                    error = e;
+                    result.append(line);
                 }
-                return null;
+                return result.toString();
+            }
+            catch (Exception e)
+            {
+                error = e;
+            }
+            return null;
+        }
+
+        @Override protected void onPostExecute(String result)
+        {
+            final Weather_Activity weak_weather = weak_ref.get();
+
+            if (result == null && error != null)
+            {
+                weak_weather.feed_failure(error);
+                return;
             }
 
-            @Override protected void onPostExecute(String result)            
-            {         
-                                
-                if (result == null && error != null)
-                {
-                    weather.feed_failure(error);
-                    return;
-                }                                
-                
-                try
-                {                                    
-                    JSONObject data = new JSONObject (result);
-                    
-                    JSONObject q_result = data.optJSONObject("query");                                        
-                    
-                    Channel chan = new Channel();
-                    chan.populate(q_result.optJSONObject("results").optJSONObject("channel"));                    
- 
-                    weather.feed_success(chan);
-                }
-                catch (JSONException e)
-                {
-                    weather.feed_failure(e);
-                } 
+            try
+            {
+                JSONObject data = new JSONObject (result);
+
+                JSONObject q_result = data.optJSONObject("query");
+
+                Channel chan = new Channel();
+                chan.populate(q_result.optJSONObject("results").optJSONObject("channel"));
+
+                weak_weather.feed_success(chan);
             }
-        }.execute(new_location); 
+            catch (JSONException e)
+            {
+                weak_weather.feed_failure(e);
+            }
+        }
     }
 }
-
-
 
 
 
